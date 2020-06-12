4e170bceef57a603e885eeb8c81c48c55f888999
onokatio <onokatio@gmail.com>
2020-06-12 18:16:11 +0900

    go fmt

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 import "math/rand"
 
-
 func main() {
 	StudentMax := 37
-	Student := make([]int,StudentMax)
+	Student := make([]int, StudentMax)
 	for index := range Student {
 		Student[index] = index + 1
 	}
@@ -15,22 +14,22 @@ func main() {
 	fmt.Print("Input Seed:")
 	fmt.Scan(&seed)
 	r := rand.New(rand.NewSource(seed))
-	r.Shuffle(StudentMax, func(i int, j int){
+	r.Shuffle(StudentMax, func(i int, j int) {
 		Student[i], Student[j] = Student[j], Student[i]
 	})
 
 	fmt.Println("Student[] = ", Student)
 
 	Committe := map[string]int{
-		"会計": 1,
-		"体育": 2,
+		"会計":   1,
+		"体育":   2,
 		"環境衛生": 2,
 		"交通安全": 2,
-		"監査": 1,
+		"監査":   1,
 	}
 
 	for Key, Role := range Committe {
-		fmt.Println(Key,": ", Student[0:Role])
+		fmt.Println(Key, ": ", Student[0:Role])
 		Student = Student[Role:]
 	}
 }
