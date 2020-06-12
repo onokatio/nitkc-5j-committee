519fb9ecf76a2ebe8e4c5864f32b7472dee0cb69
onokatio <onokatio@gmail.com>
2020-06-12 18:50:16 +0900

    add binary build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME=委員会割り当て
+
+GOOS=(linux darwin windows)
+GOARCH=(amd64 386)
+for os in ${GOOS[@]}; do
+	for arch in ${GOARCH[@]}; do
+		if [ "$os" == "windows" ];then
+			ext=".exe"
+		fi
+
+		if [ "$os" == "darwin" ];then
+			os_string="Mac-OSX"
+		else
+			os_string=$os
+		fi
+
+		if [ "$arch" == "amd64" ];then
+			arch_string="64bit"
+		else
+			arch_string="32bit"
+		fi
+
+		GOOS=$os GOARCH=$arch go build -o dst/$NAME-$os_string-${arch_string}${ext} main.go
+	done
+done
